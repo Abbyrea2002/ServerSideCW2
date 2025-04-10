30b79f0ca6ff8b587de07e80a99330efaafb371e
Abby Rea <rea_humphrys-a@ulster.ac.uk>
2025-04-10 12:23:51 +0100

    update

--- app/Http/Resources/BooksResource.php
+++ app/Http/Resources/BooksResource.php
@@ -6,14 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class BooksResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
+{ 
+    public function toArray(Request $request): array 
+    { 
         return [ 
             'id' => $this->id, 
             'title' => $this->title, 
@@ -24,5 +19,5 @@ class BooksResource extends JsonResource
             'updated_at' => $this->updated_at->toDateTimeString(), 
         ]; 
     } 
-
 }
+
